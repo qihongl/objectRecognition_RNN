3cd6fc9d5eaef6588d7fe07e49b21c2da53bda8f
Qihong Lu <Qihong@Qihongs-MacBook-Air.local>
2015-05-20 23:05:23 -0500

    fixing the patternGenerator

--- patternGen/patternMaker.m
+++ patternGen/patternMaker.m
@@ -10,9 +10,14 @@ filenameTest = fopen('allStimuli.txt','w');
 
 % some parameters
 parameters.seed = 2;
-rng(parameters.seed); % replicability... 
-parameters.visualThres = 0.9;
-parameters.verbalThres = 1;
+rng(parameters.seed); % for replicability % TODO consider move it to patternGen
+
+% the threshold is "P(being ON)" for all unit that "suppose" to be on
+% (defined by the prototype)
+parameters.visualThres = 0.9;   
+% verbal threshold is always on. For basic and superordiante category, name
+% should be the same for all members of the category
+parameters.verbalThres = 1; 
 
 
 % modeling parameters
@@ -25,6 +30,8 @@ parameters.min = 0.5;
 parameters.max = 3;
 parameters.grace = 0.5;
 
+
+
 %% get patterns
 [visualPatterns, numCategoryVis] = patternGen(protoName, parameters.visualThres);
 [verbalPatterns, numCategoryVer] = patternGen(protoName, parameters.verbalThres);
@@ -33,64 +40,64 @@ visualNames = nameGen(numCategoryVis);
 verbalNames = nameGen(numCategoryVer);
 
 
-%% write to file
-writeParameters(filename, parameters, numCategoryVis);
-writeParameters(filenameTest, parameters, numCategoryVis);
-
-% verbal -> visual
-addTitle(filename, '# verbal sup -> visual sup features' )
-names = addPrefix('verbal', verbalNames, 'sup');
-writeAllPatterns(filename, names, stimulusLength, verbalPatterns.sup, visualPatterns.sup, 2)
-clear names;
-
-addTitle(filename, '# verbal bas -> visual bas + sup features' )
-names = addPrefix('verbal', verbalNames, 'bas');
-writeAllPatterns(filename, names, stimulusLength, verbalPatterns.bas, visualPatterns.bas_sup, 2)
-clear names;
-
-addTitle(filename, '# verbal sub -> visual full features' )
-names = addPrefix('verbal', verbalNames, 'sub');
-writeAllPatterns(filename, names, stimulusLength, verbalPatterns.sub, visualPatterns.full, 2)
-clear names;
-
-
-% write visual full -> verbal full (for tempDyn)
-addTitle(filename, '# visual full -> verbal full' )
-names = addPrefix('visual', visualNames, '');
-writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, verbalPatterns.full, 1)
-writeAllPatterns(filenameTest, names, stimulusLength, visualPatterns.full, verbalPatterns.full, 1)
-clear names;
-
-
-% write visual full to 3 levels of names
-addTitle(filename, '# visual full -> verbal sup' )
-names = addPrefix('visual', visualNames, 'sup');
-writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, verbalPatterns.sup, 1)
-clear names;
-
-addTitle(filename, '# visual full -> verbal bas' )
-names = addPrefix('visual', visualNames, 'bas');
-writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, verbalPatterns.bas, 1)
-clear names;
-
-addTitle(filename, '# visual full -> verbal sub' )
-names = addPrefix('visual', visualNames, 'sub');
-writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, verbalPatterns.sub, 1)
-clear names;
-
-
-
-
-
-
-%% manipulate frequency
-
-addTitle(filename, '# visual full -> verbal bas' )
-names = addPrefix('visual', visualNames, 'bas');
-writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, verbalPatterns.bas, 1)
-clear names;
-
-addTitle(filename, '# verbal bas -> visual bas + sup features' )
-names = addPrefix('verbal', verbalNames, 'bas');
-writeAllPatterns(filename, names, stimulusLength, verbalPatterns.bas, visualPatterns.bas_sup, 2)
-clear names;
+% %% write to file
+% writeParameters(filename, parameters, numCategoryVis);
+% writeParameters(filenameTest, parameters, numCategoryVis);
+% 
+% % verbal -> visual
+% addTitle(filename, '# verbal sup -> visual sup features' )
+% names = addPrefix('verbal', verbalNames, 'sup');
+% writeAllPatterns(filename, names, stimulusLength, verbalPatterns.sup, visualPatterns.sup, 2)
+% clear names;
+% 
+% addTitle(filename, '# verbal bas -> visual bas + sup features' )
+% names = addPrefix('verbal', verbalNames, 'bas');
+% writeAllPatterns(filename, names, stimulusLength, verbalPatterns.bas, visualPatterns.bas_sup, 2)
+% clear names;
+% 
+% addTitle(filename, '# verbal sub -> visual full features' )
+% names = addPrefix('verbal', verbalNames, 'sub');
+% writeAllPatterns(filename, names, stimulusLength, verbalPatterns.sub, visualPatterns.full, 2)
+% clear names;
+% 
+% 
+% % write visual full -> verbal full (for tempDyn)
+% addTitle(filename, '# visual full -> verbal full' )
+% names = addPrefix('visual', visualNames, '');
+% writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, verbalPatterns.full, 1)
+% writeAllPatterns(filenameTest, names, stimulusLength, visualPatterns.full, verbalPatterns.full, 1)
+% clear names;
+% 
+% 
+% % write visual full to 3 levels of names
+% addTitle(filename, '# visual full -> verbal sup' )
+% names = addPrefix('visual', visualNames, 'sup');
+% writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, verbalPatterns.sup, 1)
+% clear names;
+% 
+% addTitle(filename, '# visual full -> verbal bas' )
+% names = addPrefix('visual', visualNames, 'bas');
+% writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, verbalPatterns.bas, 1)
+% clear names;
+% 
+% addTitle(filename, '# visual full -> verbal sub' )
+% names = addPrefix('visual', visualNames, 'sub');
+% writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, verbalPatterns.sub, 1)
+% clear names;
+% 
+% 
+% 
+% 
+% 
+% 
+% %% manipulate frequency
+% 
+% addTitle(filename, '# visual full -> verbal bas' )
+% names = addPrefix('visual', visualNames, 'bas');
+% writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, verbalPatterns.bas, 1)
+% clear names;
+% 
+% addTitle(filename, '# verbal bas -> visual bas + sup features' )
+% names = addPrefix('verbal', verbalNames, 'bas');
+% writeAllPatterns(filename, names, stimulusLength, verbalPatterns.bas, visualPatterns.bas_sup, 2)
+% clear names;
