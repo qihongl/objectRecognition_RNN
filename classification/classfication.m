5831694df92102a1a9da6fcab28bb69c8a907251
Qihong Lu <lvqihong1992@gmail.com>
2015-06-07 20:07:55 -0500

    done

--- classification/classfication.m
+++ classification/classfication.m
@@ -1,4 +1,4 @@
-%% superordinate classfication 
+%% superordinate classfication
 % the OVERALL GOAL of this program is to convert a 'neural response' from
 % ANN (in the form of a time series) and outputs a 'class' that represents
 % a superordinate level category
@@ -13,54 +13,46 @@ clear ; close all; clc
 data = load('data.mat');
 data = data.activationMatrix;
 
-%% do the 1st time point! LOOP if i want all time points 
-data = data{4};
-X = data(: , 1 : (size(data,2) - 1)); 
-y = data(:, size(data,2));
-
-
-%% Compute Cost and Gradient
-
-%  Setup the data matrix appropriately, and add ones for the intercept term
-[m, n] = size(X);
-
-% Add intercept term to x and X_test
-X = [ones(m, 1) X];
-
-% Initialize fitting parameters
-initial_theta = zeros(n + 1, 1);
-
-% Compute and display initial cost and gradient
-[cost, grad] = costFunction(initial_theta, X, y);
-
-fprintf('Cost at initial theta (zeros): %f\n', cost);
-fprintf('Gradient at initial theta (zeros): \n');
-fprintf(' %f \n', grad);
-
-
-%% Optimizing using fminunc
-%  In this exercise, you will use a built-in function (fminunc) to find the
-%  optimal parameters theta.
-
-%  Set options for fminunc
-options = optimset('GradObj', 'on', 'MaxIter', 400);
-
-%  Run fminunc to obtain the optimal theta
-%  This function will return theta and the cost 
-[theta, cost] = ...
-	fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
-
-% Print theta to screen
-% fprintf('Cost at theta found by fminunc: %f\n', cost);
-% fprintf('theta: \n');
-% fprintf(' %f \n', theta);
-
-
-%% Predict and Accuracies 
-
-% Compute accuracy on our training set
-p = predict(theta, X);
-
-fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
-
-
+%% do the 1st time point! LOOP if i want all time points
+accuracy = nan(size(data,1),1);
+for i = 1:size(data,1)
+    partialdata = data{i};
+    X = partialdata(: , 1 : (size(partialdata,2) - 1));
+    y = partialdata(:, size(partialdata,2));
+    
+    
+    %% Compute Cost and Gradient
+    %  Setup the data matrix appropriately, and add ones for the intercept term
+    [m, n] = size(X);
+    
+    % Add intercept term to x and X_test
+    X = [ones(m, 1) X];
+    
+    % Initialize fitting parameters
+    initial_theta = zeros(n + 1, 1);
+    
+    % Compute and display initial cost and gradient
+    [cost, grad] = costFunction(initial_theta, X, y);
+    
+    %% Optimizing using fminunc
+    %  In this exercise, you will use a built-in function (fminunc) to find the
+    %  optimal parameters theta.
+    
+    %  Set options for fminunc
+    options = optimset('GradObj', 'on', 'MaxIter', 400);
+    
+    %  Run fminunc to obtain the optimal theta
+    %  This function will return theta and the cost
+    [theta, cost] = ...
+        fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+    
+    
+    %% Predict and Accuracies
+    % Compute accuracy on our training set
+    p = predict(theta, X);
+    acc = mean(double(p == y)) * 100;
+    
+    accuracy(i) = acc;
+    fprintf('Train Accuracy: %f\n', acc);
+    
+end
--- classification/costFunction.m
+++ classification/costFunction.m
@@ -6,25 +6,13 @@ function [J, grad] = costFunction(theta, X, y)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-    h = sigmoid(X * theta)
-    J = (1/m) * sum((-y) .* log(h) - (1 - y) .* log(1 - h))
-    
-    grad = (1 / m) * (X' * (h - y))
+h = sigmoid(X * theta);
+J = (1/m) * sum((-y) .* log(h) - (1 - y) .* log(1 - h));
+grad = (1 / m) * (X' * (h - y));
 
 
-% =============================================================
 
 end
