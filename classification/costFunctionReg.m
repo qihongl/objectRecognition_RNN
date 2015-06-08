c71a9705d6070229e7d94b3effb8ea1b5a9eac57
Qihong Lu <lvqihong1992@gmail.com>
2015-06-08 00:46:26 -0500

    uncomment

--- classification/costFunctionReg.m
+++ classification/costFunctionReg.m
@@ -2,33 +2,20 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 n = size(X,2);
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+h = sigmoid(X * theta);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
+J = ((1/m) * sum((-y) .* log(h) - (1 - y) .* log(1 - h)))...
+    + (lambda / (2 * m)) * sum(theta(2:n,:) .^2);
 
-    h = sigmoid(X * theta)
-    
-    J = ((1/m) * sum((-y) .* log(h) - (1 - y) .* log(1 - h)))...
-        + (lambda / (2 * m)) * sum(theta(2:n,:) .^2)
-    
+grad(1,:) = (1 / m) * (X(:,1)' * (h - y));
+grad(2:n,:) = ((1 / m) * (X(:,2:n)' * (h - y)))...
+    + theta(2:n,:) * (lambda / m);
 
-    grad(1,:) = (1 / m) * (X(:,1)' * (h - y))
-    grad(2:n,:) = ((1 / m) * (X(:,2:n)' * (h - y)))...
-        + theta(2:n,:) * (lambda / m)
-
-
-% =============================================================
 
 end
