33fd00417c13c09d82890d8eacff7c729b818a6f
Qihong Lu <lvqihong1992@gmail.com>
2015-11-30 23:22:38 -0600

    done another simulation

--- RSA/RSA_temporal.m
+++ RSA/RSA_temporal.m
@@ -6,8 +6,8 @@
 clear all; clc; clf; close all;
 PATH.ABS = '/Users/Qihong/Dropbox/github/categorization_PDP/';
 % provide the NAMEs of the data files (user need to set them mannually)
-PATH.DATA= 'sim22.0_RSVP';
-FILENAME.VERBAL = 'hiddenAll_e3.txt';
+PATH.DATA= 'sim23.1_noise';
+FILENAME.VERBAL = 'hiddenAll_e2.txt';
 FILENAME.PROTOTYPE = 'PROTO.xlsx';
 EPOCH = 2000;
 
