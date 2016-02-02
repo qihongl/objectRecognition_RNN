af231f3d642b4e7e404fc4d6d7a6bed292af61ef
Qihong Lu <lvqihong1992@gmail.com>
2016-02-01 22:52:07 -0600

    run a group of classifiers

---
+++ classification/runGroup.m
@@ -0,0 +1,31 @@
+%% run a bunch of classifier
+% when simulating using random subset or normal noise, there is randomness
+% so we need a sample to establish average
+
+sampleSize = 10;
+timePoints = 25;
+
+% preallocate 
+group.accuracy = nan(timePoints,sampleSize);
+group.deviation = nan(timePoints,sampleSize);
+group.response = nan(timePoints,sampleSize);
+for i = 1 : sampleSize
+    % run classifier and compute average
+    [gs, param, logParams] = runClassifier(0);
+    
+    % record the average scores in 3 matrices
+    group.accuracy(:,i) = gs.averageScore.accuracy;
+    group.deviation(:,i) = gs.averageScore.deviation;
+    group.response(:,i) = gs.averageScore.response;
+    
+end
+
+%% save the results
+dataDirName = sprintf('groupScores/');
+if exist(dataDirName,'dir') ~= 7
+    mkdir(dataDirName)
+else
+    warning('Directory exists!')
+end
+dataFileName = sprintf('gs_subsetProp%.3d', logParams.subsetProp * 100);
+save([dataDirName '/' dataFileName],'group')
