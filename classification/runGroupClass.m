073f7bb210167bd03c69baaeb32c752e88992966
Qihong Lu <lvqihong1992@gmail.com>
2016-02-14 14:49:07 -0600

    classification - run both conditions

--- classification/runGroupClass.m
+++ classification/runGroupClass.m
@@ -1,7 +1,7 @@
 %% run a bunch of classifier
 % when simulating using random subset or normal noise, there is randomness
 % so we need a sample to establish average
-
+clear variables; clc; 
 sampleSize = 20;
 timePoints = 25;
 
@@ -16,36 +16,42 @@ FILENAME.PROTOTYPE = 'PROTO.xlsx';
 %% parameter for logistic regresison classifier
 % classOpt = classification options
 propChoice = [.005 .01 .05 .15 .3 1];
-logParam.classOpt = 'spatBlurring';
+optionChoice = {'randomSubset', 'spatBlurring'};
+
 % variance of the normal noise
 logParam.variance = 0;
 
-for p = 1: length(propChoice);
-    logParam.subsetProp = propChoice(p);
+% loop over all option choices
+for opt = 1 : length(optionChoice)    
+    logParam.classOpt = optionChoice{opt};
+    % loop over all proportion parameters
     
-    %% run the analysis
-    % preallocate
-    group.accuracy = nan(timePoints,sampleSize);
-    group.deviation = nan(timePoints,sampleSize);
-    group.response = nan(timePoints,sampleSize);
-    for i = 1 : sampleSize
-        % run classifier and compute average
-        [gs, param] = runClassifier(logParam,PATH,FILENAME,0);
+    for p = 1: length(propChoice);
+        logParam.subsetProp = propChoice(p);
         
-        % record the average scores in 3 matrices
-        group.accuracy(:,i) = gs.averageScore.accuracy;
-        group.deviation(:,i) = gs.averageScore.deviation;
-        group.response(:,i) = gs.averageScore.response;
+        %% run the analysis
+        % preallocate
+        group.accuracy = nan(timePoints,sampleSize);
+        group.deviation = nan(timePoints,sampleSize);
+        group.response = nan(timePoints,sampleSize);
+        for i = 1 : sampleSize
+            % run classifier and compute average
+            [gs, param] = runClassifier(logParam,PATH,FILENAME,0);
+            % record the average scores in 3 matrices
+            group.accuracy(:,i) = gs.averageScore.accuracy;
+            group.deviation(:,i) = gs.averageScore.deviation;
+            group.response(:,i) = gs.averageScore.response;
+        end
         
+        %% read metadata
+        dataDirName = sprintf('groupScores_class/');
+        subDataDirName = getSubDataDirName(PATH,FILENAME);
+        finalSavePath = strcat(dataDirName,subDataDirName);
+        %% save the results
+        checkAndMkdir(dataDirName);
+        checkAndMkdir(finalSavePath);
+        
+        dataFileName = sprintf('gsClass_%s%.3d.mat', logParam.classOpt, logParam.subsetProp * 100);
+        save([finalSavePath '/' dataFileName],'group')
     end
-    
-    %% save the results
-    dataDirName = sprintf('groupScores_class/');
-    if exist(dataDirName,'dir') ~= 7
-        mkdir(dataDirName)
-    else
-        warning('Directory exists!')
-    end
-    dataFileName = sprintf('gsClass_%s%.3d.mat', logParam.classOpt, logParam.subsetProp * 100);
-    save([dataDirName '/' dataFileName],'group')
 end
