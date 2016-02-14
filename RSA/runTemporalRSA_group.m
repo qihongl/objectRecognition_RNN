594e0f6579369f9d4d54ed1a83269f385aaf213f
Qihong Lu <lvqihong1992@gmail.com>
2016-02-14 14:49:58 -0600

    save RDMs also

--- RSA/runTemporalRSA_group.m
+++ RSA/runTemporalRSA_group.m
@@ -1,35 +1,48 @@
 %% compute a sample of RDM correlation scores (for statistics)
-
+clear all; clc; clf;
 %% parameters
-param.option = 'spatBlurring';
-param.porp = 1; 
-sampleSize = 10;
+param.option = 'randomSubset';
+param.propChoices = [.01, .05, .15, .3, 1];
+sampleSize = 20;
 
-%% path info
-PATH.ABS = '/Users/Qihong/Dropbox/github/categorization_PDP/';
-% provide the NAMEs of the data files (user need to set them mannually)
-PATH.DATA= 'sim23.1_noise';
-FILENAME.VERBAL = 'hiddenAll_e1.txt';
-FILENAME.PROTOTYPE = 'PROTO.xlsx';
-
-%% Run group analysis
-% preallocate fields
-timePoints = 25;
-corr.basic = nan(timePoints, sampleSize);
-corr.super = nan(timePoints, sampleSize);
-% run a sample of temporal RDM simulations
-for i = 1 : sampleSize
-    corrData = runTemporalRSA(param, PATH, FILENAME);
-    corr.basic(:,i) = corrData.basic';
-    corr.super(:,i) = corrData.super';
+for p = 1 : length(param.propChoices)
+    % loop over all choices of proportion parameter
+    param.porp = param.propChoices(p)
+    
+    %% path info
+    PATH.ABS = '/Users/Qihong/Dropbox/github/categorization_PDP/';
+    % provide the NAMEs of the data files (user need to set them mannually)
+    PATH.DATA= 'sim23.3_noise';
+    FILENAME.VERBAL = 'hiddenAll_e2.txt';
+    FILENAME.PROTOTYPE = 'PROTO.xlsx';
+    
+    %% Run group analysis
+    % preallocate fields
+    timePoints = 25;
+    corr.basic = nan(timePoints, sampleSize);
+    corr.super = nan(timePoints, sampleSize);
+    % run a sample of temporal RDM simulations
+    RDMs = cell(sampleSize,1);
+    for i = 1 : sampleSize
+        % RDMs: each cell has a time series for a diff. "subject"
+        [corrData RDMs{i}] = runTemporalRSA(param, PATH, FILENAME);
+        
+        % corr: time by subject correlation matrix
+        corr.basic(:,i) = corrData.basic';
+        corr.super(:,i) = corrData.super';
+    end
+    
+    %% save the results
+    dataDirName = sprintf('groupScores_RSA/');
+    if exist(dataDirName,'dir') ~= 7
+        mkdir(dataDirName)
+    else
+        warning('Directory exists!')
+    end
+    % save time x subject correlational data file
+    corrDataFileName = sprintf('gRSA_%s%.3d', param.option, param.porp * 100);
+    save([dataDirName '/' corrDataFileName],'corr')
+    % save RDMs
+    rdmDataFileName = sprintf('gRDM_%s%.3d', param.option, param.porp * 100);
+    save([dataDirName '/' rdmDataFileName],'RDMs')
 end
-
-%% save the results
-dataDirName = sprintf('groupScores_RSA/');
-if exist(dataDirName,'dir') ~= 7
-    mkdir(dataDirName)
-else
-    warning('Directory exists!')
-end
-dataFileName = sprintf('gRSA_%s%.3d', param.option, param.porp * 100);
-save([dataDirName '/' dataFileName],'corr')
