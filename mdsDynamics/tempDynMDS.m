6aa79b4ebdb1ebd66c0161cfdc808379f53d6766
Qihong Lu <lvqihong1992@gmail.com>
2016-03-20 00:21:06 -0500

    pltting CMDS solutions over time

--- mdsDynamics/tempDynMDS.m
+++ mdsDynamics/tempDynMDS.m
@@ -1,7 +1,6 @@
-%% plot MDS solution over time
-% [D,Z] = procrustes(X,Y(:,1:2)); % TODO: can be used to fit raw visual embedding
-%% CONSTANTS
-clear variables; clf; close all; clc; 
+%% Plot MDS solution over time
+% initialization
+clear variables; clf; close all; clc;
 PATH.PROJECT = '/Users/Qihong/Dropbox/github/categorization_PDP/';
 % provide the NAMEs of the data files (user need to set them mannually)
 % PATH.SIMID= 'sim16_large';
@@ -11,14 +10,14 @@ FILENAME.ACT = 'hiddenAll_e2.txt';
 FILENAME.PROTOTYPE = 'PROTO.xlsx';
 
 % set parameters
-timePt = 5;         % select from int[1,25]
+timePt = 1;         % select from int[1,25]
 turnOnAxis = false;
 attachLabels = true;
 
 %% read data
 % fixed parameters
-nTimePts = 25;    % you probably don't want to change it.. 
-% read prototype and parameters 
+nTimePts = 25;    % you probably don't want to change it..
+% read prototype and parameters
 PATH.PROTOTYPE = genDataPath(PATH, FILENAME.PROTOTYPE);
 [param, prototype] = readPrototype(PATH.PROTOTYPE);
 nObjs = param.numStimuli;
@@ -27,44 +26,52 @@ nObjs = param.numStimuli;
 PATH.DATA = genDataPath(PATH, FILENAME.ACT);
 data = dlmread(PATH.DATA,' ', 0,2);
 data(:,size(data,2)) = []; % last column are zeros (reason unknown...)
-data(1 + (0:nObjs-1)*(nTimePts+1),:) = []; % remove zero rows 
+data(1 + (0:nObjs-1)*(nTimePts+1),:) = []; % remove zero rows
 
 %% compute 2 dimensional MDS
 % get unique numbers in the distance matrix
-tempIdx = timePt + ((0:nObjs-1) * nTimePts);
-dist.num = pdist(data(tempIdx,:));
-dist.matrix = squareform(dist.num);
-imagesc(dist.matrix); colormap(jet);
-
+dist.num = pdist(data);
 % compute the multidimensional scaling
 [Y,evals] = cmdscale(dist.num);
+Y = Y(:,1:2);
 
-%% plot it! 
+%% plot it!
 % set plotting constants
 SCALE = 1.2;
 FS = 16;
 LW = 3;
 
-% plot 
-plot(Y(:,1),Y(:,2),'bx', 'linewidth', LW);
-% rescale the size of the panel
+hold on
+for itemNum = 1 : nObjs
+    tempIdx = (1 + (itemNum-1) * nTimePts) : (itemNum*nTimePts);
+    plot(Y(tempIdx,1),Y(tempIdx,2), 'b')
+    plot(Y(tempIdx,1),Y(tempIdx,2), 'b.')
+end
+
+initIdx = 1 + (0:nObjs-1) * nTimePts;
+plot(Y(initIdx,1),Y(initIdx,2), 'rx', 'linewidth',3)
+hold off
+
+% square form plot 
 axis(max(max(abs(Y))) * [-1,1,-1,1] * SCALE); axis('square');
 
-title_text = sprintf('Classical Metric Multidimensional Scaling [time point == %d]', timePt);
+% add title
+title_text = 'Classical Metric Multidimensional Scaling over time';
 title(title_text, 'fontsize', FS)
-
 set(gca,'FontSize',FS-1)
 
-%% other elements in the plots
+
 if attachLabels
     labels = nameGen(param.numCategory);
     for i = 1 : length(labels)
-        labels(i) = sliceStrings(char(labels(i)));
+        tempSlices = strsplit(labels{i},'_');
+        labels{i} = tempSlices(1);
     end
+    
+    finalIdx = nTimePts + (0:nObjs-1) * nTimePts;
+    text(Y(finalIdx,1),Y(finalIdx,2)+0.05* max(max(abs(Y))),labels, ...
+        'HorizontalAlignment','left', 'fontsize', FS);
 end
-% attach labels and shift up the label by 5% of "max distance"
-text(Y(:,1),Y(:,2)+0.05* max(max(abs(Y))),labels, ...
-    'HorizontalAlignment','left', 'fontsize', FS);
 
 if turnOnAxis
     line([-1,1],[0 0],'XLimInclude','off','Color',[.7 .7 .7])
