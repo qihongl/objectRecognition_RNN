7bf6931874ec4ccfa21f1cd88d810dc8496f4ff0
Qihong Lu <lvqihong1992@gmail.com>
2016-03-30 21:43:51 -0500

    re-plot final t RSA

--- RSA/RSA.m
+++ RSA/RSA.m
@@ -1,8 +1,8 @@
-% initialize 
+% initialize
 clear all;close all;clc
 path.root = '/Users/Qihong/Dropbox/github/categorization_PDP/';
-path.dir = 'sim22.2_RSVP';
-path.file = 'hiddenFinal_e4.txt';
+path.dir = 'sim23.2_noise';
+path.file = 'hiddenFinal_e2.txt';
 filename = [path.root path.dir '/' path.file];
 
 %% start
@@ -10,12 +10,33 @@ filename = [path.root path.dir '/' path.file];
 X = dlmread(filename,' ', 0, 3);
 % compute RDM
 RDM = computeRDM(X);
-% plot it 
+% plot it
 colormap jet
 img = imagesc(RDM);
-colorbar
+set(gca, 'XTick', [], 'YTick', [],'dataAspectRatio',[1 1 1])
+
+% h=colorbar;
+% set(h,'fontsize',14);
+% h.Ticks = [];
+
 title_text = sprintf('%s-%s',path.dir, path.file);
-title(title_text)
+% title(title_text)
+
+%%
+% remove the tick labels:
+
+
+
+
+
+%% customize ticks
+% ax = gca;
+% ax.XTick = 1:48;
+% ax.YTick = 1:48;
+% ax.XTickLabel = {'1','2','3','4'};
+% ax.YTickLabel = {'1','2','3','4'};
+
+
 
 %%
 % imname = sprintf('RDM%.3d', 1);
