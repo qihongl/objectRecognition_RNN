2a45e77a0dafb15b3cd47c57bb4b9b31c9b91700
Qihong Lu <lvqihong1992@gmail.com>
2016-09-11 22:19:55 -0500

    increase the basic level frequency

--- patternGen/patternMaker.m
+++ patternGen/patternMaker.m
@@ -1,10 +1,10 @@
 %% Generate the stimuli file for my PDP model for semantics
-clear; clc; close all; 
+clear; clc; close all;
 % CONSTANTS
 PATH.PROJECT = '/Users/Qihong/Dropbox/github/categorization_PDP/patternGen';
 
 %% Parameters
-% get full patterns 
+% get full patterns
 protoName = 'PROTO3.xlsx';
 % write to a file
 filename = fopen('environment.txt','w');
@@ -16,13 +16,13 @@ rng(parameters.seed);% TODO consider move it to patternGen
 
 % the threshold is "P(being ON)" for all unit that "suppose" to be on
 % (defined by the prototype)
-parameters.visualThres = 1;   
+parameters.visualThres = 1;
 % verbal threshold is always on. For basic and superordiante category, name
 % should be the same for all members of the category
-parameters.verbalThres = 1; 
+parameters.verbalThres = 1;
 
 % modeling parameters
-stimulusLength = 15;     
+stimulusLength = 15;
 parameters.defI = 0;
 parameters.defT = NaN;
 parameters.actI = 1;
@@ -35,14 +35,14 @@ allTargetTypes = {'visual_sup', 'visual_bas_sup', 'visual_all', ...
     'verbal_none', 'verbal_sup',  'verbal_bas', 'verbal_sub'};
 
 %% get patterns
-[protoParam, prototype] = readPrototype (protoName); 
+[protoParam, prototype] = readPrototype (protoName);
 [visualPatterns] = patternGen(protoParam, prototype, parameters.visualThres);
 [verbalPatterns] = patternGen(protoParam, prototype, parameters.verbalThres);
 % generate names for all patterns
 visualPatterns.names = nameGen(visualPatterns.numCategory);
 verbalPatterns.names = nameGen(verbalPatterns.numCategory);
 
-%% save the actual pattern used 
+%% save the actual pattern used
 saveParams(parameters, visualPatterns, verbalPatterns, protoParam, prototype)
 
 %% write all patterns to a file
@@ -61,7 +61,7 @@ addTitle(filename, '# verbal bas -> visual bas + sup features' )
 targetType = 'visual_bas+sup';
 names = addPrefix('verbal', verbalPatterns.names, 'bas');
 writeAllPatterns(filename, names, stimulusLength, verbalPatterns.bas, ...
-        visualPatterns.sup_bas, protoParam, targetType, 2)
+    visualPatterns.sup_bas, protoParam, targetType, 2)
 clear names;
 
 addTitle(filename, '# verbal sub -> visual full features' )
@@ -109,19 +109,22 @@ clear names;
 
 
 %% manipulate frequency (add more basic training patterns)
-targetType = 'verbal_bas';
-
-addTitle(filename, '# visual full -> verbal bas' )
-names = addPrefix('visual', visualPatterns.names, 'bas');
-writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, ... 
-    verbalPatterns.bas, protoParam, targetType, 1)
-clear names;
-
-addTitle(filename, '# verbal bas -> visual bas + sup features' )
-names = addPrefix('verbal', verbalPatterns.names, 'bas');
-writeAllPatterns(filename, names, stimulusLength, verbalPatterns.bas, ...
-    visualPatterns.sup_bas, protoParam, targetType, 2)
-clear names;
+freq = 10; 
+for f = 1 : freq
+    targetType = 'verbal_bas';
+    addTitle(filename, '# visual full -> verbal bas' )
+    names = addPrefix('visual', visualPatterns.names, 'bas');
+    writeAllPatterns(filename, names, stimulusLength, visualPatterns.full, ...
+        verbalPatterns.bas, protoParam, targetType, 1)
+    clear names;
+    
+    targetType = 'visual_bas+sup';
+    addTitle(filename, '# verbal bas -> visual bas + sup features' )
+    names = addPrefix('verbal', verbalPatterns.names, 'bas');
+    writeAllPatterns(filename, names, stimulusLength, verbalPatterns.bas, ...
+        visualPatterns.sup_bas, protoParam, targetType, 2)
+    clear names;
+end
 
 
 disp('Done!')
