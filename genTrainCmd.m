0702dd035d13db570033209ef587e3f677979a10
Qihong Lu <lvqihong1992@gmail.com>
2016-10-09 12:02:13 -0500

    gen training and testing cmd for lens

--- genTrainCmd.m
+++ genTrainCmd.m
@@ -1,20 +1,32 @@
-clear all; clc 
+clear all; clc
+
+trainModel = 0;
 
 modelName = 'network.txt';
 procFileName = '../procs.tcl';
-trainFileName = 'train.txt'; 
-trainLength = 200; 
-maxEpoch = 2000; 
+trainFileName = 'train.txt';
+conditions = {'normal', 'rapid'};
+condition = conditions{1};
+trainLength = 200;
+maxEpoch = 2000;
 
 
-allstages = ''; 
+allstages = '';
 for epochs = trainLength: trainLength: maxEpoch
-    temp_train_text = sprintf('train %d; saveWeights e%.2d.wt -text; ', trainLength, epochs/100);
-    temp_test_text = sprintf('testAllActs verbalAll_e%.2d.txt VerbalRep; testAllActs hiddenAll_e%.2d.txt hidden; ', epochs/100, epochs/100);
+    if trainModel
+        temp_train_text = sprintf('train %d; saveWeights e%.2d.wt -text; ', trainLength, epochs/100);
+    else
+        temp_train_text = sprintf('loadWeights e%.2d.wt; ', epochs/100);
+    end
+    temp_test_text = sprintf('testAllActs verbal_%s_e%.2d.txt VerbalRep; testAllActs hidden_%s_e%.2d.txt hidden; ', ...
+        condition, epochs/100, condition, epochs/100);
     reload_text = sprintf('loadExamples %s; ', trainFileName);
     onestage = strcat(temp_train_text, temp_test_text, reload_text);
     allstages = strcat(allstages, onestage);
 end
 output = sprintf('lens -n -c %s " source %s; %s exit"', modelName, procFileName, allstages);
+
 fprintf(output)
 fprintf('\n')
+
+
