9040c61115e8cecb7ed16fdde0cdc54038c06126
Qihong Lu <lvqihong1992@gmail.com>
2017-03-24 18:26:27 -0500

    take a snapshot before re-org files...

--- advantages/compute_mean_prob.m
+++ advantages/compute_mean_prob.m
@@ -7,10 +7,12 @@ avg.sup_base = mean(baseline.sup,2);
 avg.bas_base = mean(baseline.bas,2);
 
 prob.sup = avg.sup ./ (avg.sup + avg.sup_base * (p.numCategory.sup-1));
-prob.bas = avg.bas ./ (avg.bas + avg.bas_base * (p.numCategory.sup*p.numCategory.bas-1));
-prob.sup_base = avg.sup_base ./ (avg.sup + avg.sup_base * (p.numCategory.sup-1));
-prob.bas_base = avg.bas_base ./ (avg.bas + avg.bas_base * (p.numCategory.sup*p.numCategory.bas-1));
+% prob.bas = avg.bas ./ (avg.bas + avg.bas_base * (p.numCategory.sup*p.numCategory.bas-1));
+prob.bas = avg.bas ./ (avg.bas + avg.bas_base * (p.numCategory.bas-1));
 
+prob.sup_base = avg.sup_base ./ (avg.sup + avg.sup_base * (p.numCategory.sup-1));
+% prob.bas_base = avg.bas_base ./ (avg.bas + avg.bas_base * (p.numCategory.sup*p.numCategory.bas-1));
+prob.bas_base = avg.bas_base ./ (avg.bas + avg.bas_base * (p.numCategory.bas-1));
 
 end
 
