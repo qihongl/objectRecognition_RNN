6e75525535db4338e8cce98c57c9a2d4d23f7875
Qihong Lu <lvqihong1992@gmail.com>
2017-06-07 21:53:36 -0500

    add subtraction plots

--- advantages/plotTempDynamics.m
+++ advantages/plotTempDynamics.m
@@ -1,17 +1,17 @@
-function [ output_args ] = plotTempDynamics(avg, prob, FILENAME)
-
-
+%% Visualize the activation profiles of the verbal layer 
+function [ output_args ] = plotTempDynamics(act, prob, FILENAME)
+% graph param 
 g.LW = 2; 
 g.FS = 14; 
 
 figure(1)
 % raw activation 
-subplot(121)
+subplot(221)
 hold on 
-plot(avg.sup, 'r', 'linewidth',g.LW)
-plot(avg.bas,'b', 'linewidth',g.LW)
-plot(avg.sup_base, '--r', 'linewidth',g.LW)
-plot(avg.bas_base, '--b','linewidth',g.LW)
+plot(act.sup, 'r', 'linewidth',g.LW)
+plot(act.bas,'b', 'linewidth',g.LW)
+plot(act.sup_base, '--r', 'linewidth',g.LW)
+plot(act.bas_base, '--b','linewidth',g.LW)
 hold off 
 title('Verbal Activation')
 xlabel('Time ticks');ylabel('Mean Activation')
@@ -19,9 +19,20 @@ legend({'Sup-target','Basic-target','Sup-nontargets','Basic-nontarget'}, 'locati
 % ylim([0 .7])
 set(gca,'FontSize',g.FS)
 
+subplot(222)
+% substraction plot 
+hold on; 
+plot(act.sup - act.sup_base, 'r', 'linewidth',g.LW); 
+plot(act.bas - act.bas_base, 'b', 'linewidth',g.LW); 
+hold off 
+title('Substraction')
+xlabel('Time ticks');ylabel('Mean Activation')
+legend({'Sup','Basic'}, 'location', 'best')
+set(gca,'FontSize',g.FS)
+
 
 % luce choice probability 
-subplot(122)
+subplot(223)
 
 hold on 
 plot(prob.sup, 'r', 'linewidth',g.LW)
@@ -30,16 +41,27 @@ plot(prob.sup_base, '--r', 'linewidth',g.LW)
 plot(prob.bas_base, '--b', 'linewidth',g.LW)
 
 hold off 
-xlabel('Time ticks');ylabel('Probability w.r.t alternatives')
+xlabel('Time ticks');
+ylabel('Probability')
 title('Luce Probability')
 legend({'Sup-target','Basic-target','Sup-nontargets','Basic-nontarget'}, 'location', 'best')
 set(gca,'FontSize',g.FS)
 
+subplot(224)
+% substraction plot 
+hold on; 
+plot(prob.sup - prob.sup_base, 'r', 'linewidth',g.LW); 
+plot(prob.bas - prob.bas_base, 'b', 'linewidth',g.LW); 
+hold off 
+title('Substraction')
+xlabel('Time ticks');
+ylabel('Probability')
+legend({'Sup','Basic'}, 'location', 'best')
+set(gca,'FontSize',g.FS)
+
+
 suptitle_text = strrep(sprintf('%s', FILENAME.ACT), '_', '-');
 suptitle(suptitle_text)
-
-
-
 
 %%
 figure(3)
